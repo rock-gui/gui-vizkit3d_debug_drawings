c7234b84d67e016a7b482eb3b20db5d89f95054c
Arne Böckmann <arne.boeckmann@dfki.de>
2017-01-10 15:10:08 +0100

    Initial commit

---
+++ src/Dummy.cpp
@@ -0,0 +1,10 @@
+#include "Dummy.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace vizkit3dDebugDrawings;
+
+void DummyClass::welcome()
+{
+    cout << "You successfully compiled and executed DummyProject. Welcome!" << endl;
+}
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vizkit3dDebugDrawings/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    vizkit3dDebugDrawings::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
