954a72cca80cafcbcebb5903ce04405c665ce161
Arne Böckmann <arne.boeckmann@dfki.de>
2017-04-18 15:25:01 +0200

    bugifx: adapt ruby script to changed lib name

--- viz/vizkit_plugin.rb
+++ viz/vizkit_plugin.rb
@@ -1,5 +1,5 @@
-Vizkit::UiLoader.register_3d_plugin('DrawingVisualization', 'vizkit3dDebugDrawings', 'DrawingVisualization')
+Vizkit::UiLoader.register_3d_plugin('DrawingVisualization', 'vizkit3d_debug_drawings', 'DrawingVisualization')
 Vizkit::UiLoader.register_3d_plugin_for('DrawingVisualization', "/vizkti3dDebugDrawings/Drawing", :updateData)
 
-Vizkit::UiLoader.register_3d_plugin('DebugDrawingVisualization', 'vizkit3dDebugCommand', 'DebugDrawingVisualization')
+Vizkit::UiLoader.register_3d_plugin('DebugDrawingVisualization', 'vizkit3d_debug_drawings_command', 'DebugDrawingVisualization')
 Vizkit::UiLoader.register_3d_plugin_for('DebugDrawingVisualization', "/boost/shared_ptr</vizkit3dDebugDrawings/CommandBuffer>", :updateData)
