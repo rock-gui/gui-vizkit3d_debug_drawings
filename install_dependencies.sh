bec3386c905582038a978c6c69bf0b27cbdcedb3
Arne Böckmann <arne.boeckmann@dfki.de>
2019-09-24 15:38:09 +0200

    standalone install script

---
+++ install_dependencies.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -e #abort with error if any command returns with something other than zero
+
+############
+# Install script for v3dd dependencies
+# Run "$> sudo ./install_dependencies.sh" to install system wide
+# Run "$> ./install_dependencies.sh <path_to_prefix>" to install to a local prefix
+############
+
+function build {
+
+  if [ -d $3 ] 
+  then
+    echo "Directory $3 already exists. " 
+    read -r -p "Do you want to delete and re-download $3? [y/N] " response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+    then
+     rm -rf $3
+    else
+      echo "Aborting!"
+      exit -1
+    fi
+  fi
+  git clone --branch $2 $1 $3
+  mkdir $3/build
+  cd $3/build
+  cmake .. $4
+  make -j install
+  cd ../../
+}
+
+
+
+PREFIX=""
+ABS_PREFIX=""
+if [ "$#" -eq 1 ]; then
+ABS_PREFIX=`readlink -f $1`
+PREFIX="-DCMAKE_INSTALL_PREFIX=$ABS_PREFIX"
+
+echo "" > env.sh #create empty env.sh
+echo "export CMAKE_PREFIX_PATH=$ABS_PREFIX" >> env.sh
+echo "export PKG_CONFIG_PATH=$ABS_PREFIX/lib/pkgconfig:$ABS_PREFIX/share/pkgconfig:$ABS_PREFIX/lib64/pkgconfig:\$PKG_CONFIG_PATH" >> env.sh
+echo "export LD_LIBRARY_PATH=$ABS_PREFIX/lib:$ABS_PREFIX/lib64:\$LD_LIBRARY_PATH" >> env.sh
+echo "export PATH=$ABS_PREFIX/bin:\$PATH" >> env.sh
+
+source env.sh
+  
+fi
+
+build https://github.com/rock-core/base-cmake.git master base-cmake "$PREFIX"
+build https://github.com/rock-core/gui-osgviz.git master gui-osgviz "$PREFIX"
+build https://github.com/orocos-toolchain/rtt.git master rtt "$PREFIX"
+build https://github.com/rock-core/gui-vizkit3d.git master gui-vizkit3d "$PREFIX"
+build https://github.com/rock-core/gui-rock_widget_collection.git master gui-rock_widget_collection "$PREFIX"
+
+
+
+
+
+
