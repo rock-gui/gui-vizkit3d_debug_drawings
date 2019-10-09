7906fa3fa9b0f2cc90cf1597461d1b0dec929078
Arne Böckmann <arne.boeckmann@dfki.de>
2019-10-09 17:51:21 +0200

    more namespace stuff

--- test/draw.hpp
+++ test/draw.hpp
@@ -11,7 +11,7 @@ void draw()
 {    
     //draw something to test buffering before configuration
     V3DD::DRAW_CYLINDER("cylinder", Eigen::Vector3d(3, 3, 2), Eigen::Vector3d(1,1,1),
-                  vizkit3dDebugDrawings::Color::android_green);
+                  V3DD::Color::android_green);
 
     Eigen::Vector4d color(1, 0, 0, 1);
     V3DD::DRAW_SPHERE("sphere", -2, 1, 1, .5, color);
@@ -74,7 +74,7 @@ void draw()
     points.emplace_back(2, 2, 2);
     V3DD::DRAW_POLYLINE("polyLine", linePos, points, lineColor);
     V3DD::DRAW_POLYLINE("polyLine2", points, lineColor);
-    V3DD::DRAW_LINE("line", Eigen::Vector3d(-1, -1, -1), Eigen::Vector3d(0, 1, 2), vizkit3dDebugDrawings::Color::cyan);
+    V3DD::DRAW_LINE("line", Eigen::Vector3d(-1, -1, -1), Eigen::Vector3d(0, 1, 2), V3DD::Color::cyan);
     
 
 
@@ -84,8 +84,8 @@ void draw()
         min << -1, -1, -1;
         max << 1, 1, 1;
         Eigen::AlignedBox3d boundingBox(min, max);
-        V3DD::DRAW_AABB("Complex", boundingBox, vizkit3dDebugDrawings::Color::alloy_orange);
-         V3DD::DRAW_SPHERE("Complex", -7, 1, 1, 1, vizkit3dDebugDrawings::Color::magenta);
+        V3DD::DRAW_AABB("Complex", boundingBox, V3DD::Color::alloy_orange);
+         V3DD::DRAW_SPHERE("Complex", -7, 1, 1, 1, V3DD::Color::magenta);
     });
     
 
@@ -108,8 +108,8 @@ void draw()
         ++i;
         
         Eigen::Vector3d blinkPos(-3, -3, -3);
-        V3DD::DRAW_SPHERE("removeTest", blinkPos, 1, vizkit3dDebugDrawings::Color::air_force_blue_raf);
-        V3DD::DRAW_SPHERE("remvoeTest2", Eigen::Vector3d(-4, -4, -4), 0.8, vizkit3dDebugDrawings::Color::cadmium_orange);
+        V3DD::DRAW_SPHERE("removeTest", blinkPos, 1, V3DD::Color::air_force_blue_raf);
+        V3DD::DRAW_SPHERE("remvoeTest2", Eigen::Vector3d(-4, -4, -4), 0.8, V3DD::Color::cadmium_orange);
         std::this_thread::sleep_for(std::chrono::milliseconds(50));
         V3DD::REMOVE_DRAWING("removeTest");
         V3DD::CLEAR_DRAWING("remvoeTest2");
